8d01251b027f3adfe6bc22c2c725ebcc5cbeb9d4
Renato Welinton Schlogel <renato.s@outlook.com>
2020-12-12 23:37:56 -0300

    seção 2 - 15. Outros recursos úteis do Mockito - Mocando com anotations

--- PrimeiroTeste/src/test/java/br/com/renatoschlogel/MockitoTests.java
+++ PrimeiroTeste/src/test/java/br/com/renatoschlogel/MockitoTests.java
@@ -1,21 +1,22 @@
 package br.com.renatoschlogel;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MockitoTests {
 
+	@Mock
+	List<String> lista;
 	
 	@Test
 	public void primeiroTesteMockito() throws Exception {
-		
-		List<String> lista = Mockito.mock(ArrayList.class);
 		
 		Mockito.when(lista.size()).thenReturn(10);
 		
