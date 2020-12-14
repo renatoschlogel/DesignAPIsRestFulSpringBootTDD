678033726f2f25becc678046613e9b903d1e12ef
Renato Welinton Schlogel <renato.s@outlook.com>
2020-12-14 00:08:13 -0300

    seção 4: Spring Boot - 26. Basico para a Criação de Uma API Rest com spring boot- parte 2

--- ApiRESTSpringBoot/src/main/java/br/com/renatoschlogel/api/MeuResource.java
+++ ApiRESTSpringBoot/src/main/java/br/com/renatoschlogel/api/MeuResource.java
@@ -1,18 +1,41 @@
 package br.com.renatoschlogel.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/clientes")
 public class MeuResource {
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente salvar(@RequestBody Cliente cliente) {
+		return cliente;
+	}
 
-	@GetMapping("/api/clientes/{id}")
-	public Cliente buscarCliente(@PathVariable Long id) {
-		
-		System.out.println(id);
-		
+	@GetMapping("/{id}")
+	public Cliente buscar(@PathVariable Long id) {
+		System.out.println(id);	
 		return new Cliente("Renato", "20121212112");
+	}
+	
+	@PutMapping
+	public Cliente atualizar(@RequestBody Cliente cliente) {
+		return cliente;
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long id) {
+		System.out.println(id);	
 	}
 	
 }
