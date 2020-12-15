e6a3ed83abbf3451ab1ba4034f4680b5c9e0f199
Renato Welinton Schlogel <renato.s@outlook.com>
2020-12-15 00:24:41 -0300

    seção 5: Projeto Library API: Livros 32. Aprendendo a desenhar uma requisição utilizando TDD

--- library-api/src/test/java/br/com/renatoschlogel/libraryapi/api/resource/BookControllerTest.java
+++ library-api/src/test/java/br/com/renatoschlogel/libraryapi/api/resource/BookControllerTest.java
@@ -1,6 +1,8 @@
 package br.com.renatoschlogel.libraryapi.api.resource;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,23 +10,49 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @WebMvcTest
 @AutoConfigureMockMvc
 public class BookControllerTest {
+	
+	static String BOOK_API = "/api/books";
 
 	@Autowired
-	MockMvc mvn;
+	MockMvc mvc;
 	
 	@Test
 	@DisplayName("Deve criar um livro com sucesso!")
-	public void createBookTest() {
+	public void createBookTest() throws Exception {
 		
+		String json = new ObjectMapper().writeValueAsString(null);
+		
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+			.post(BOOK_API)
+			.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON)
+			.content("");
+		
+		mvc.perform(request)
+		   .andExpect(status().isCreated())
+		   .andExpect(jsonPath("id").isNotEmpty())
+		   .andExpect(jsonPath("title").value("GO TEAM!"))
+		   .andExpect(jsonPath("author").value("Autor"))
+		   .andExpect(jsonPath("isbn").value("123"))
+		   ;
+				  
+				  ;
 	}
 	
 	@Test
