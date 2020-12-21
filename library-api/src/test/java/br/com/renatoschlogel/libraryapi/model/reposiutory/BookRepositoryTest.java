457cfa27df684291b5dd511fbb3b24d27d214de0
Renato Welinton Schlogel <renato.s@outlook.com>
2020-12-21 00:17:45 -0300

    seção 5: Projeto Library API: Livros - 42. Criando nosso primeiro teste de integração com base de dados

---
+++ library-api/src/test/java/br/com/renatoschlogel/libraryapi/model/reposiutory/BookRepositoryTest.java
@@ -0,0 +1,39 @@
+package br.com.renatoschlogel.libraryapi.model.reposiutory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import br.com.renatoschlogel.libraryapi.model.repository.BookRepository;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
+public class BookRepositoryTest {
+
+	@Autowired
+	TestEntityManager entityManager;
+	
+	@Autowired
+	BookRepository bookRepository;
+	
+	@Test
+	@DisplayName("Deve retornar verdadeiro quando existir um livro na base com o isbn informado")
+	void returnTrueWhenIsbnExists() throws Exception {
+		
+		String isbn = "123";
+		
+		boolean existsByIsbn = bookRepository.existsByIsbn(isbn);
+		
+		assertThat(existsByIsbn).isTrue();
+	}
+}
