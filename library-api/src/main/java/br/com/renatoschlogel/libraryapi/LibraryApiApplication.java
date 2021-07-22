707ddd07389dce1e58b62c1f0d4f9fb5afbb7fc9
Renato Welinton Schlogel <renato.s@outlook.com>
2021-07-22 12:40:33 -0300

    removendo codigo temporario de teste do envio de email

--- library-api/src/main/java/br/com/renatoschlogel/libraryapi/LibraryApiApplication.java
+++ library-api/src/main/java/br/com/renatoschlogel/libraryapi/LibraryApiApplication.java
@@ -1,17 +1,10 @@
 package br.com.renatoschlogel.libraryapi;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import br.com.renatoschlogel.libraryapi.service.EmailService;
 
 @SpringBootApplication
 @EnableScheduling
