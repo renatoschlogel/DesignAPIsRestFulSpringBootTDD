5d0f4ea45b2caf13e29061340a57a11b8a4c5fbf
Renato Welinton Schlogel <renato.s@outlook.com>
2021-07-22 11:07:06 -0300

    Sessão 7: Sub-recursos, Envio de Emails e Agendamento de tarefas com Spring 83. Implementando o serviço de agendamento de tarefas

--- library-api/src/main/java/br/com/renatoschlogel/libraryapi/LibraryApiApplication.java
+++ library-api/src/main/java/br/com/renatoschlogel/libraryapi/LibraryApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
@@ -15,12 +14,6 @@ public class LibraryApiApplication {
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
-	
-	@Scheduled(cron = "0 22 1 1/1 * ?")
-	public void testeAgendamentoTarefaz() {
-		System.out.println("Agendamento Funcionando");
-	}
-	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
